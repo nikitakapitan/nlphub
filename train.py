4183637db75a84d72b0f7d5714e093a2b15ac67f
Nikita Kapchenko <kapchenko.nikita@gmail.com>
2023-11-07 14:42:12 +0100

    update train

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ import logging
 import argparse
 import time
 from transformers import TrainingArguments, Trainer
+from transformers import AutoTokenizer
 from nlphub import FineTuner
 
 # Initialize logging
@@ -29,7 +30,8 @@ def main(args):
     finetuner = FineTuner(config)
 
     # TOKENIZE
-    tokenize = lambda batch: finetuner.tokenizer(batch['text'], truncation=True)
+    # finetuner.tokenizer = AutoTokenizer.from_pretrained(config['BASE_MODEL_NAME'])
+    tokenize = lambda batch: finetuner.tokenizer(batch['text'], padding='max_length', truncation=True)
     dataset_encoded = finetuner.dataset.map(tokenize, batched=True)
 
     # Training Arguments
@@ -39,6 +41,7 @@ def main(args):
         learning_rate=config['LEARNING_RATE'],
         per_device_train_batch_size=config['BATCH_SIZE'],
         per_device_eval_batch_size=config['BATCH_SIZE'],
+        warmup_steps=500,
         weight_decay=0.01,
         evaluation_strategy='epoch',
         disable_tqdm=False,
@@ -52,27 +55,16 @@ def main(args):
         args=training_args,
         train_dataset=dataset_encoded['train'],
         eval_dataset=dataset_encoded[config['EVAL_DATASET']],
-        tokenizer=finetuner.tokenizer,
         compute_metrics=finetuner.compute_metrics_func,
     )
+  
+    print("Start TRAINING")
+    trainer.train()
+    trainer.evaluate()
 
-    # Train and Evaluate
-    try:
-        print("Start TRAINING")
-        trainer.train()
-        trainer.evaluate()
-    except Exception as e:
-        print(f"Error during training or evaluation: {e}")
-        exit(1)
-
-    # Push to Hub
-    try:
-        print("PUSH MODEL TO THE HUB")
-        trainer.push_to_hub()
-        logging.info("Model pushed to Hugging Face Hub.")
-    except Exception as e:
-        print(f"Error pushing to Hugging Face Hub: {e}")
-        exit(1)
+    print("PUSH MODEL TO THE HUB")
+    trainer.push_to_hub()
+    logging.info("Model pushed to Hugging Face Hub.")
 
 
 if __name__ == '__main__':
